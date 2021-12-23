dbdf9bcf172322ab097e241a4dd03d81462f9ea9
Adrian Cybulski <adrian@cybulski.cc>
2021-12-23 10:00:50 +0000

    Synchronise KV store

--- pkg/storage/kv/kv.go
+++ pkg/storage/kv/kv.go
@@ -1,30 +1,44 @@
 package kv
 
 import (
-	"encoding/json"
+	"sync"
 
+	"github.com/Reverse-Labs/storage/pkg/encoder"
 	"github.com/Reverse-Labs/storage/pkg/storage"
 )
 
 type KV struct {
-	Storage storage.Provider
+	storage   storage.Provider
+	marshaler encoder.Marshaler
+	lock      sync.Mutex
+}
+
+func New(s storage.Provider, m encoder.Marshaler) KV {
+	return KV{storage: s, marshaler: m}
 }
 
 func (kv *KV) Put(key string, v interface{}) error {
-	b, err := json.Marshal(v)
+	kv.lock.Lock()
+	defer kv.lock.Unlock()
+
+	b, err := kv.marshaler.Marshal(v)
+
 	if err != nil {
 		return err
 	}
 
-	return kv.Storage.WriteBytes(key, b)
+	return kv.storage.WriteBytes(key, b)
 }
 
 func (kv *KV) Get(key string, v interface{}) error {
-	b, err := kv.Storage.ReadBytes(key)
+	kv.lock.Lock()
+	defer kv.lock.Unlock()
+
+	b, err := kv.storage.ReadBytes(key)
 
 	if err != nil {
 		return err
 	}
 
-	return json.Unmarshal(b, v)
+	return kv.marshaler.Unmarshal(b, v)
 }
